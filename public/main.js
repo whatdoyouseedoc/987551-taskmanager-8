fa8bf36df9a9cb88ca4a901b5260e11cf6f15a01
Mac <mac@MacBook-Pro.local>
2019-02-20 22:06:13 +0300

    Refactoring

--- public/main.js
+++ public/main.js
@@ -337,14 +337,16 @@ const getCardMarkup = (color, modifier, text = '') => `
 
 const $cards = document.querySelector('.board__tasks');
 
+/** Render one card */
 const renderCard = (markup) => {
     $cards.innerHTML += markup;
 };
 
-const renderCards = cardsAmount => {
+/** Render all cards */
+const renderCards = amount => {
     flushCards();
 
-    for (let i = 0; i < cardsAmount; i++) {
+    for (let i = 0; i < amount; i++) {
         cards.push(getCardMarkup(
             colors[getRandInt(0, 4)],
             modifiers[getRandInt(0, 2)],
@@ -364,21 +366,20 @@ const flushCards = () => {
 };
 
 let cards = [];
-const cardsAmount = 7;
 
 const colors = ['pink', 'yellow', 'blue', 'black'];
 const modifiers = ['card--deadline', 'card--repeat', ''];
 const texts = ['Train dog', 'Need to learn JS', 'Sleep well', 'Buy more synths', 'Lorem ipsum', ''];
 const hashTagsMock = ['#one', '#one #two', ''];
 
-/* Main */
-renderFilters();
-
-renderCards(7);
-
+/* Filter change handler */
 const changeFilter = amount => {
     renderCards(amount);
 };
+
+/* Main */
+renderFilters();
+renderCards(7);
 
 /* Utils */
 function getRandInt(min, max) {
