2f2c7d3cc75f47a22e344cfe1b2239d2bd4c2053
Alexander Ermakov <Alexander.Ermakov@sidenis.com>
2019-02-22 13:46:02 +0300

    Setup dev-server complete

--- webpack.config.js
+++ webpack.config.js
@@ -7,5 +7,11 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.join(__dirname, 'public')
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'public'),
+    publicPath: 'http://localhost:8080/',
+    hot: true,
+    compress: true
   }
 };
