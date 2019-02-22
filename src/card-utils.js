f60d37164101d302411e941b765d157282f84343
Alexander Ermakov <Alexander.Ermakov@sidenis.com>
2019-02-22 13:34:18 +0300

    Modules separated.

---
+++ src/card-utils.js
@@ -0,0 +1,30 @@
+import {getRandInt} from './utils';
+import cardMarkup from './card-markup';
+
+const colors = [`pink`, `yellow`, `blue`, `black`];
+const modifiers = [`card--deadline`, `card--repeat`, ``];
+const texts = [`Train dog`, `Need to learn JS`, `Sleep well`, `Buy more synths`, `Lorem ipsum`, ``];
+const hashTagsMock = [`#one`, `#one #two`, ``];
+
+const renderCard = (selector, markup) => {
+  selector.innerHTML += markup;
+};
+
+const flushCards = (selector) => {
+  selector.innerHTML = ``;
+};
+
+const renderCards = (selector, amount) => {
+  flushCards(selector);
+
+  for (let i = 0; i < amount; i++) {
+    renderCard(selector, cardMarkup(
+      colors[getRandInt(0, 4)],
+      modifiers[getRandInt(0, 2)],
+      texts[getRandInt(0, 5)],
+      hashTagsMock[getRandInt(0, 2)]
+    ));
+  }
+}
+
+export { renderCards };
--- src/main.js
+++ src/main.js
@@ -1,45 +1,14 @@
 import {renderFilters} from './filter-utils';
-import {colors, modifiers, texts, hashTagsMock} from './card-props';
-import cardMarkup from './card-markup';
-
-import {getRandInt} from './utils';
+import {renderCards} from './card-utils';
 
 const $filters = document.querySelector(`.main__filter`);
 const $cards = document.querySelector(`.board__tasks`);
 
-let cards = [];
-
-const renderCard = (markup) => {
-  $cards.innerHTML += markup;
-};
-
-function renderCards(amount) {
-  flushCards();
-
-  while (cards.length < amount) {
-    cards.push(cardMarkup(
-      colors[getRandInt(0, 4)],
-      modifiers[getRandInt(0, 2)],
-      texts[getRandInt(0, 5)],
-      hashTagsMock[getRandInt(0, 2)]
-    ));
-  }
-
-  cards.forEach((item) => {
-    renderCard(item);
-  });
-}
-
-const flushCards = () => {
-  cards = [];
-  $cards.innerHTML = ``;
-};
-
 /* Main */
 renderFilters($filters);
-renderCards(7);
+renderCards($cards, 7);
 
 $filters.addEventListener('change', ($event) => {
   const count = $event.target.attributes['data-count'].value;
-  renderCards(count);
+  renderCards($cards, count);
 });
